81c4e19fa2f991e2807ca0b5879656d0144e6447
002369DXChen <114504959+002369DXChen@users.noreply.github.com>
2026-06-20 21:11:52 +0800

    supercar补全
    补全L/R操作和相关test

--- executor/src/sports_car_state.rs
+++ executor/src/sports_car_state.rs
@@ -1,11 +1,11 @@
-use crate::action::Action;
+use crate::action::{self, Action};
 use crate::assembler::Assembler;
 
 #[derive(Default, Copy, Clone)]
 
 pub(crate) struct SportsCarState {
     is_reverse: bool,
-    is_fast: bool,    
+    is_fast: bool,
 }
 
 impl Assembler for SportsCarState {
@@ -16,7 +16,7 @@ impl Assembler for SportsCarState {
         actions.push(Action::Forward(direction));
         actions.push(Action::Forward(direction));
 
-        if self.is_fast{
+        if self.is_fast {
             actions.push(Action::Forward(direction));
             actions.push(Action::Forward(direction));
         }
@@ -24,11 +24,41 @@ impl Assembler for SportsCarState {
         actions
     }
     fn turn_left_assemble(&self) -> Vec<Action> {
-        Vec::new()
+        let mut actions = Vec::new();
+        let direction = if self.is_reverse { -1 } else { 1 };
+
+        if self.is_fast {
+            actions.push(Action::Forward(direction));
+        }
+
+        actions.push(if self.is_reverse {
+            Action::TurnRight
+        } else {
+            Action::TurnLeft
+        });
+
+        actions.push(Action::Forward(direction));
+
+        actions
     }
 
     fn turn_right_assemble(&self) -> Vec<Action> {
-        Vec::new()
+        let mut actions = Vec::new();
+        let direction = if self.is_reverse { -1 } else { 1 };
+
+        if self.is_fast {
+            actions.push(Action::Forward(direction));
+        }
+
+        actions.push(if self.is_reverse {
+            Action::TurnLeft
+        } else {
+            Action::TurnRight
+        });
+
+        actions.push(Action::Forward(direction));
+
+        actions
     }
 
     fn be_reverse(&mut self) {
--- executor/tests/sports_car_test.rs
+++ executor/tests/sports_car_test.rs
@@ -1,6 +1,6 @@
 use executor::{Pose, SportsCarExecutor};
 
-mod sports_car_move_tasts {
+mod sports_car_move_tests {
     use super::*;
 
     #[test]
@@ -30,5 +30,68 @@ mod sports_car_move_tasts {
         executor.execute("FBM");
         assert_eq!(executor.query(), Pose::new(-4, 0, 'E'));
     }
-    
+}
+
+mod sports_car_turn_left_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_y_plus_1_and_facing_n_given_command_is_l_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("L");
+        assert_eq!(executor.query(), Pose::new(0, 1, 'N'));
+    }
+
+    #[test]
+    fn should_return_y_plus_1_and_facing_s_given_command_is_bl_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("BL");
+        assert_eq!(executor.query(), Pose::new(0, 1, 'S'));
+    }
+
+    #[test]
+    fn should_return_x_plus_1_y_plus_1_and_facing_n_given_command_is_fl_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("FL");
+        assert_eq!(executor.query(), Pose::new(1, 1, 'N'));
+    }
+
+    #[test]
+    fn should_return_x_minus_1_y_plus_1_and_facing_s_given_command_is_fbl_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("FBL");
+        assert_eq!(executor.query(), Pose::new(-1, 1, 'S'));
+    }
+}
+
+mod sports_car_turn_right_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_y_minus_1_and_facing_s_given_command_is_r_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("R");
+        assert_eq!(executor.query(), Pose::new(0, -1, 'S'));
+    }
+
+    #[test]
+    fn should_return_y_minus_1_and_facing_n_given_command_is_br_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("BR");
+        assert_eq!(executor.query(), Pose::new(0, -1, 'N'));
+    }
+
+    #[test]
+    fn should_return_x_plus_1_y_minus_1_and_facing_s_given_command_is_fr_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("FR");
+        assert_eq!(executor.query(), Pose::new(1, -1, 'S'));
+    }
+
+    #[test]
+    fn should_return_x_minus_1_y_minus_1_and_facing_n_given_command_is_fbr_and_facing_is_e() {
+        let mut executor = SportsCarExecutor::with_pose(Pose::new(0, 0, 'E'));
+        executor.execute("FBR");
+        assert_eq!(executor.query(), Pose::new(-1, -1, 'N'));
+    }
 }
